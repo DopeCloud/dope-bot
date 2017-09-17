96b3c12e41714889b1ec114cd035c8e8a57e5356
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-09-17 00:24:30 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,17 +7,17 @@ const config = require("./config.json");
 // CONNECT MYSQL!
 // ==========================================================
 const connection = mysql.createConnection({
-  host: '145.239.xxx.xxx',
-  user: 'dope_xxxx_xxxxx',
-  password: 'dope*xxxxxx',
-  database: 'dope_xxxxxx'
+  host: '69.xx.xx.xx',
+  user: 'user_xxxxxxx',
+  password: 'pw_xxxx',
+  database: 'dope_xxxx'
 });
 // ==========================================================
 
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.setGame(`DopeHosting.net`);
 });
 
 client.on("guildCreate", guild => {
@@ -30,6 +30,15 @@ client.on("guildDelete", guild => {
   client.user.setGame(`on ${client.guilds.size} servers`);
 });
 
+client.on('guildMemberAdd', member => {
+    let guild = member.guild;
+    guild.channels.find("name", "general").send(`Welcome To DopeHosting ${member.user} Please Take A look at #Welcome !`);
+});
+
+client.on('guildMemberRemove', member => {
+    let guild = member.guild;
+    guild.channels.find("name", "general").send(`R.I.P ${member.user}. !`);
+});
 
 client.on("message", async message => {
   if(message.author.bot) return;
@@ -51,8 +60,27 @@ client.on("message", async message => {
   // ==========================================================
   if(command === "say") {
     const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
+    message.delete().catch(O_o=>{});
     message.channel.send(sayMessage);
+  }
+  // ==========================================================
+
+  // ==========================================================
+  // Help.
+  // ==========================================================
+  if(command === "help") {
+    message.channel.send(
+        `${message.author}` + "Available Commands |"
+        + "\n" + '|~*~*~*~*~*~*~*~*~*~*~*~*~*~*~~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~|'
+        + "\n" + '|~*~*~*~*~*~*~*~*~*~*~*~*~*~*~~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~|'
+        + "\n" + '|!ping           - Simple ping                             ' 
+        + "\n" + '|!say             - Say something                          ' 
+        + "\n" + '|!cc               - Count Clients                         ' 
+        + "\n" + '|!p?              - Pending Orders                         ' 
+        + "\n" + '|!hosted      - Hosted Domains By DopeHosting              ' 
+        + "\n" + '|!kick           - Kick Someone.                           '
+        + "\n" + '|~*~*~*~*~*~*~*~*~*~*~*~*~*~*~~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~|'
+        + "\n" + '|~*~*~*~*~*~*~*~*~*~*~*~*~*~*~~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~|');
   }
   // ==========================================================
 
@@ -60,38 +88,50 @@ client.on("message", async message => {
   // COUNT CLIENTS.
   // ==========================================================
   if(command === "cc") {
+      message.delete().catch(O_o=>{});
       const m = await message.channel.send("Counting Clients.. Please Wait!");
       
       connection.query('SELECT * FROM tblclients', (err,rows) => {
         if(err) throw err;
-        m.edit(`Total Clients: **${rows.length}**`);
+        m.edit(`Total Clients : **${rows.length}**`);
        });
   }
   // ==========================================================
 
   // ==========================================================
-  // MYSQL STATUS.
+  // PENDING ORDERS.
   // ==========================================================
-  if(command === "mstatus?") {
-      const m = await message.channel.send("Please Wait!");
+  if(command === "p?") {
+      message.delete().catch(O_o=>{});
+      const m = await message.channel.send("Counting Pending Orders.. Please Wait!");
       
-      connection.connect((err) => {
-       if(err){
-              m.edit('LoL Mysql Server DOWN!');
-              return;
-       }
-       m.edit(`Connected! in ${m.createdTimestamp - message.createdTimestamp}ms. **MYSQL SRV ONLINE!**`);
-      });
+      connection.query('SELECT `status` FROM `tblorders` WHERE `status`="pending"', (err,rows) => {
+        if(err) throw err;
+        m.edit(`Pending Orders : **${rows.length}**`);
+       });
   }
   // ==========================================================
     
+  // ==========================================================
+  // HOSTED.
+  // ==========================================================
+  if(command === "hosted") {
+      message.delete().catch(O_o=>{});
+      const m = await message.channel.send("Counting Pending Orders.. Please Wait!");
+      
+      connection.query('SELECT * FROM `tblhosting`', (err,rows) => {
+        if(err) throw err;
+        m.edit(`Hosted Domains : **${rows.length}**`);
+       });
+  }
+  // ==========================================================
     
   // ==========================================================
   // Kick Someone !kick @user reason
   // ==========================================================
   if(command === "kick") {
     if(!message.member.roles.some(r=>["HEAD", "DEV-TEAM"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
+      return message.reply("Sorry, you don't have permissions to use this :facepalm: !");
     let member = message.mentions.members.first();
     if(!member)
       return message.reply("Please mention a valid member of this server");
